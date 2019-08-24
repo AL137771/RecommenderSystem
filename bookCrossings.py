539c9a16e971090ea19617a1c9961d8f680c36a8
lilCaesarxXx <ces.pertel@gmail.com>
2019-08-24 04:02:56 -0600

    Se agrego codigo

--- bookCrossings.py
+++ bookCrossings.py
@@ -25,3 +25,20 @@ books.loc[books.ISBN == '078946697X','yearOfPublication'] = 2000
 books.loc[books.ISBN == '078946697X','bookAuthor'] = "Michael Teitelbaum"
 books.loc[books.ISBN == '078946697X', 'publisher'] = "DK Publishing Inc"
 books.loc[books.ISBN == '078946697X', 'bookTitle' ] = "Dk Readers: Creating the X-Men, How it all began(level 4, Proficient Readers)"
+
+books.loc[books.ISBN == '2070426769','yearOfPublication'] = 2003
+books.loc[books.ISBN == '2070426769','bookAuthor'] = "Jean Marie Gustave Le"
+books.loc[books.ISBN == '2070426769', 'publisher'] = "Gallimard"
+books.loc[books.ISBN == '2070426769', 'bookTitle' ] = "Peuple de ciel, sulvi de 'Les Bergers'"
+
+
+
+books.yearOfPublication=pd.to_numeric(books.yearOfPublication, errors='coerce')
+
+# sorted(books['yearOfPublication'].unique())
+
+
+books.loc[(books.yearOfPublication > 2006)|(books.yearOfPublication == 0), 'yearOfPublication'] = np.NAN
+books.yearOfPublication.fillna(round(books.yearOfPublication.mean()), inplace=True)
+
+books.yearOfPublication = books.yearOfPublication.astype(np.int32)
